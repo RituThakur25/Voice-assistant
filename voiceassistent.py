068453d177a0150fe295178c55436701917879ba
RituThakur25 <rituthakurhp85@gmail.com>
2024-06-14 10:12:18 -0500

    Add files via upload

---
+++ voiceassistent.py
@@ -0,0 +1,16 @@
+# import pyttsx3
+# engine= pyttsx3.init("sapi5")
+
+# voices = engine.getProperty("voices")
+# engine.setProperty("voice",voices[1].id)
+
+# engine.setProperty("rate",170)
+# engine.setProperty("volume",80)
+# engine.setProperty("pitch",2.0)
+
+
+# def speak(audio):
+#     engine.say(audio)
+#     engine.runAndWait()
+
+# speak("hello everyone, i am ritu thakur")
